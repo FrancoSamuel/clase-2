e77a39ac80f331d1ce66a2e2e75ad60cfa971bf4
FrancoSamuel <103118316+FrancoSamuel@users.noreply.github.com>
2022-04-06 22:29:14 -0300

    a
    a

---
+++ clase2.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as pl
+x = np.arange(0, 11, 1)
+y = x
+print('Values of x: ', x)
+print('Values of y: ', y)
+plt.plot(x, y)
+plt.title("Identity Function")
+plt.xlabel("Values of x")
+plt.ylabel("Values of y")
+plt.show()
